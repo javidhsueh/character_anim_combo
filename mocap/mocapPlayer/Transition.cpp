06c6ace8103482efffb7164d6c1cb387b84ef8a5
javid <javidhsueh@gmail.com>
2013-06-07 17:31:31 -0700

    finish transition, but now it's still strange at the slerp of bone rotation

---
+++ mocap/mocapPlayer/Transition.cpp
@@ -0,0 +1,9 @@
+//
+//  Transition.cpp
+//  mocap
+//
+//  Created by Javid on 6/6/13.
+//  Copyright (c) 2013 javid. All rights reserved.
+//
+
+#include "Transition.h"
---
+++ mocap/mocapPlayer/Transition.h
@@ -0,0 +1,14 @@
+//
+//  Transition.h
+//  mocap
+//
+//  Created by Javid on 6/6/13.
+//  Copyright (c) 2013 javid. All rights reserved.
+//
+
+#ifndef mocap_Transition_h
+#define mocap_Transition_h
+
+
+
+#endif
