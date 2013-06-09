48c9bfe5ac40dfdd3fed22016c8fc7575686f18a
javid <javidhsueh@gmail.com>
2013-06-08 20:55:23 -0700

    Add Floyd algorithm, modify UI, implement transition

---
+++ mocap/mocapPlayer/Floyd.cpp
@@ -0,0 +1,9 @@
+//
+//  Floyd.cpp
+//  mocap
+//
+//  Created by Javid on 6/7/13.
+//  Copyright (c) 2013 javid. All rights reserved.
+//
+
+#include "Floyd.h"
---
+++ mocap/mocapPlayer/Floyd.h
@@ -0,0 +1,14 @@
+//
+//  Floyd.h
+//  mocap
+//
+//  Created by Javid on 6/7/13.
+//  Copyright (c) 2013 javid. All rights reserved.
+//
+
+#ifndef mocap_Floyd_h
+#define mocap_Floyd_h
+
+
+
+#endif
